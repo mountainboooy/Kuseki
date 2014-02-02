069196c52d2be8536f3a2e13b0b947cd3275216b
Takeru Yohihara <yoshiharatakeru@gmail.com>
2014-02-02 17:37:45 +0900

    保存結果一覧に必要項目追加

--- Kuseki/ViewController/SavedResultViewController.m
+++ Kuseki/ViewController/SavedResultViewController.m
@@ -74,6 +74,22 @@
 }
 
 
+- (UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"header0"];
+    UILabel *lb_month_day = (UILabel*)[cell viewWithTag:1];
+    UILabel *lb_dep_stn = (UILabel*)[cell viewWithTag:2];
+    UILabel *lb_arr_stn = (UILabel*)[cell viewWithTag:3];
+    
+    lb_month_day.text = [NSString stringWithFormat:@"%@月%@日",_condition.month, _condition.day];
+    lb_dep_stn.text = _condition.dep_stn;
+    lb_arr_stn.text = _condition.arr_stn;
+    
+    return cell;
+    
+}
+
+
 - (void)updateCell:(UITableViewCell*)cell atIndexPath:(NSIndexPath*)indexPath
 {
     if(_responseManager.responses.count == 0){
@@ -82,35 +98,100 @@
     
     KUResponse * response = _responseManager.responses[indexPath.row];
     
+    
     //name
     UILabel *lb_name = (UILabel*)[cell viewWithTag:1];
     lb_name.text = response.name;
     
     //dep_time
     UILabel *lb_dep_time = (UILabel*)[cell viewWithTag:2];
+    lb_dep_time.text = [NSString stringWithFormat:@"%@発", response.dep_time];
     
     //arr_time
     UILabel *lb_arr_time = (UILabel*)[cell viewWithTag:3];
+    lb_arr_time.text = [NSString stringWithFormat:@"%@着", response.arr_time];
     
     //ec_ns
     UIImageView *iv_ec_ns = (UIImageView*)[cell viewWithTag:4];
+    iv_ec_ns.image = [self imgForSeatValue:response.seat_ec_ns];
     
     //ec_s
     UIImageView *iv_ec_s = (UIImageView*)[cell viewWithTag:5];
+    iv_ec_s.image = [self imgForSeatValue:response.seat_ec_s];
     
     //gr_ns
     UIImageView *iv_gr_ns = (UIImageView*)[cell viewWithTag:6];
+    iv_gr_ns.image = [self imgForSeatValue:response.seat_gr_ns];
     
     //gr_s
     UIImageView *iv_gr_s = (UIImageView*)[cell viewWithTag:7];
-    
-    
+    iv_gr_s.image = [self imgForSeatValue:response.seat_gr_s];
     
 }
 
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 65;
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 35;
+}
+
+
+#pragma mark -
+#pragma mark button action
 
 - (IBAction)btBackPressed:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
 }
+
+
+
+#pragma mark -
+#pragma mark private method
+
+- (UIImage*)imgForSeatValue:(enum KUSheetValue)seatValue
+{
+    NSString *imgName;
+    
+    switch (seatValue) {
+            
+        case SEAT_VACANT:{
+            imgName = @"seat_vacant";
+            break;
+        }
+            
+        case SEAT_BIT:{
+            imgName = @"seat_bit";
+            break;
+        }
+            
+        case SEAT_FULL:{
+            imgName = @"seat_full";
+            break;
+        }
+            
+        case SEAT_INVALID:{
+            imgName = @"seat_invalid";
+            break;
+        }
+            
+        case SEAT_NOT_EXIST_SMOKINGSEAT:{
+            imgName = @"seat_not_exist_smokingseat";
+            break;
+        }
+            
+        default:
+            break;
+    }
+    
+    
+    return [UIImage imageNamed:imgName];
+}
+
 
 @end
