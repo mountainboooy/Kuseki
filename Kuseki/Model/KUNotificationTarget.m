ee03642e226f59c992487c1e71a0d3534a9bfa7a
Takeru Yohihara <yoshiharatakeru@gmail.com>
2014-02-04 20:48:09 +0900

    途中

--- Kuseki/Model/KUNotificationTarget.m
+++ Kuseki/Model/KUNotificationTarget.m
@@ -8,7 +8,7 @@
 
 #import "KUNotificationTarget.h"
 #import "KUDBClient.h"
-#import "KUResponse.h"
+#import "KUResponse.h
 
 @implementation KUNotificationTarget
 
@@ -92,6 +92,114 @@
 {
     //あとで
 }
+
+
+
+- (void)getNewResponses
+{
+    
+    //condition
+    KUSearchCondition *targetCondition;
+    NSArray *savedConditions = [[KUDBClient sharedClient]selectAllConditions];
+    
+    //対応する検索条件をDBから取得
+    for (KUSearchCondition *savedCondition in savedConditions) {
+        if ([savedCondition.identifier isEqualToString:_condition_id]) {
+            targetCondition  = savedCondition;
+        }
+    }
+    
+    
+    
+    //url
+    NSURL  *base_url = [NSURL URLWithString:@"http://www1.jr.cyberstation.ne.jp/"];
+    NSString *path = @"csws/Vacancy.do";
+    
+    //param
+    NSDictionary *param = @{@"month":condition.month,
+                            @"day":condition.day,
+                            @"hour":condition.hour,
+                            @"minute":condition.minute,
+                            @"train":condition.train,
+                            @"dep_stn":condition.dep_stn,
+                            @"arr_stn":condition.arr_stn
+                            };
+    
+    
+    KUClient *client = [[KUClient alloc]initWithBaseUrl:base_url];
+    
+    [client postPath:path param:param completion:^(NSString *dataString) {
+        
+        [self setInfoWithBodyData:dataString];
+        if (completion) {
+            completion();
+        }
+        
+    } failure:^(NSHTTPURLResponse *res, NSError *error) {
+        if (failure) {
+            failure();
+        }
+        
+    }];
+}
+
+
+
+
+- (NSArray*)setInfoWithBodyData:(NSString*)bodyData
+{
+    //TODO:ここでのエラーハンドリングが重要
+    
+    NSError *err = nil;
+    HTMLParser *parser = [[HTMLParser alloc]initWithString:bodyData error:&err];
+    
+    if (err) {
+        return nil;
+    }
+    
+    HTMLNode *bodyNode = [parser body];
+    
+    NSArray *tableNodes = [bodyNode findChildTags:@"table"];
+    NSArray *trNodes;
+    
+    
+    for (HTMLNode *tableNode in tableNodes) {//テーブルの中の<tr>の要素を抽出
+        if ([[tableNode getAttributeNamed:@"border"]isEqualToString:@"3"]) {
+            trNodes = [tableNode findChildTags:@"tr"];
+        }
+    }
+    
+    
+    
+    for (HTMLNode *trNode in trNodes) {
+        
+        if ([trNodes indexOfObject:trNode] > 1 ) {
+            
+            NSArray *tdNodes = [trNode findChildTags:@"td"];
+            
+            if (tdNodes.count == 7) {
+                
+                //モデルクラス作成
+                NSDictionary *response = @{@"name":[tdNodes[0] contents],
+                                           @"dep_time":[tdNodes[1] contents],
+                                           @"arr_time":[tdNodes[2] contents],
+                                           @"seat_ec_ns":[tdNodes[3] contents],
+                                           @"seat_ec_s":[tdNodes[4] contents],
+                                           @"seat_gr_ns":[tdNodes[5] contents],
+                                           @"seat_gr_s":[tdNodes[6] contents]
+                                           };
+                
+                KUResponse *new_response = [[KUResponse alloc]initWithDictionary:response];
+                [self addResponse:new_response];
+            }
+        }
+    }
+    
+    return [NSArray array];
+    
+}
+
+
 
 
 
