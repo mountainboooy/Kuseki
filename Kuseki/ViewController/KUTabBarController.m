8b74663ac4cfa561d71a7aad91771e244d3b504a
Takeru Yoshihara <takeru@notainc.com>
2016-02-26 21:09:07 +0900

    tabarのアイコンを変更

--- Kuseki/ViewController/KUTabBarController.m
+++ Kuseki/ViewController/KUTabBarController.m
@@ -15,6 +15,12 @@
 @implementation KUTabBarController
 
 - (void)viewWillAppear:(BOOL)animated {
+    
+    // selection color
+    UIColor *selectionColor = [UIColor colorWithRed:0.16 green:0.21 blue:0.30 alpha:1.0];
+    [[UITabBar appearance] setTintColor:selectionColor];
+    
+    // localize
     [super viewWillAppear:animated];
     UITabBarItem *searchPage = self.tabBar.items[0];
     searchPage.title = NSLocalizedString(@"search", nil);
