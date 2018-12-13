03806c4c3402d678a364951a4de7cda7322b076d
Takeru Yoshihara <takeru@notainc.com>
2018-12-14 00:05:52 +0900

    広告を配置

--- Kuseki/ViewController/ResultsViewController.m
+++ Kuseki/ViewController/ResultsViewController.m
@@ -17,6 +17,7 @@
 #import "Flurry.h"
 #import "KUStationsManager.h"
 #import "KUReviewMusterController.h"
+@import GoogleMobileAds;
 
 @interface ResultsViewController ()
 <UITableViewDataSource, UITableViewDelegate>
@@ -25,6 +26,7 @@
     //outlet
     __weak IBOutlet UITableView *_tableView;
     __weak IBOutlet UIButton *_btSave;
+    __weak IBOutlet GADBannerView *bannerView;
     
     //model
     KUSearchCondition    *_condition;
@@ -42,6 +44,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    //admob
+    self->bannerView.adUnitID = @"ca-app-pub-3940256099942544/2934735716";
+    self->bannerView.rootViewController = self;
+    [self->bannerView loadRequest:[GADRequest request]];
     
     //tableView
     _tableView.dataSource = self;
